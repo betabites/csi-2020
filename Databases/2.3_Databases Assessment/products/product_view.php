e735c1aa46d50e331eb46354be989e1e67953449
Jack Hawinkels <jack.hawinkels@gmail.com>
2020-06-30 11:52:39 +1200

    Fixed image names and added to variations view

--- Databases/2.3_Databases Assessment/products/product_view.php
+++ Databases/2.3_Databases Assessment/products/product_view.php
@@ -29,17 +29,30 @@ $variations = $frame->get_product_variations($product["product_id"]);
         <?php
         $variations_output = [];
         foreach ($variations as $i => $variation) {
+            if ($variation["size"] != "" and $variation["colour"] != "") {
+                $variation["title"] = $variation["size"]." - ".$variation["colour"];
+            } elseif ($variation["size"] != "") {
+                $variation["title"] = $variation["size"];
+            } elseif ($variation["colour"] != "") {
+                $variation["title"] = $variation["colour"];
+            } else {
+                $variation["title"] = "";
+            }
             array_push($variations_output, $variation);
+
             if ($i == 0) {
                 echo "<img src='/../assets/images/".$variation["img_location"]."' style='width:100%;' id='primary_image' /><br><br><strong>Variations:</strong><br>";
-                echo "<img src='/../assets/images/".$variation["img_location"]."' class='subitem' id='pro_variation_".$i."' onclick='change_variation(".$i.")' style='box-shadow: 0 2px 5px black; z-index: 2'>";
+                echo "<img src='/../assets/images/".$variation["img_location"]."' class='subitem' id='pro_variation_".$i."' onclick='change_variation(".$i.")' style='box-shadow: 0 2px 5px black; z-index: 2' title='".$variation["title"]."' alt='".$variation["title"]."'>";
             } else {
-                echo "<img src='/../assets/images/".$variation["img_location"]."' class='subitem' id='pro_variation_".$i."' onclick='change_variation(".$i.")'>";
+                echo "<img src='/../assets/images/".$variation["img_location"]."' class='subitem' id='pro_variation_".$i."' onclick='change_variation(".$i.")' title='".$variation["title"]."' alt='".$variation["title"]."'>";
             }
 
         }
+        echo "<div id='variation_name'>".$variations_output[0]["title"]."</div>";
+        echo "<div id='variations'>";
+        echo "</div>";
+        echo "<h1 id='price'>$".$variations_output[0]["price"]."</h1>";
         ?>
-        <h1>2XL</h1>
         <script>
             let variations = <?php echo json_encode($variations_output); ?>;
             let current_variation = 0
@@ -50,6 +63,8 @@ $variations = $frame->get_product_variations($product["product_id"]);
                 document.getElementById('pro_variation_' + current_variation).style.zIndex = ""
                 document.getElementById('pro_variation_' + variation_id).style.boxShadow = "0 2px 5px black";
                 document.getElementById('pro_variation_' + variation_id).style.zIndex = "2"
+                document.getElementById("variation_name").innerHTML = variations[variation_id]["title"]
+                document.getElementById("price").innerHTML = "$" + variations[variation_id]["price"]
 
                 current_variation = variation_id
 
