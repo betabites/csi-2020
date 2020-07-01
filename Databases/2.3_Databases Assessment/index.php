5781a77667752f68f1a2f776272d21f0ea2cbbe0
Jack Hawinkels <jack.hawinkels@gmail.com>
2020-07-01 13:22:56 +1200

    Updated mainpage header

--- Databases/2.3_Databases Assessment/index.php
+++ Databases/2.3_Databases Assessment/index.php
@@ -22,10 +22,26 @@ $frame = new frame();
                 </div>
                 <h1>Kool Kiwiana</h1>
                 <div id="mainpage_about">
-                    <h1>About <strong>Kool Kiwiana</strong></h1>
+                    <h1 style="font-size: 100pt; margin-bottom: 0;">Kool Kiwiana</h1>
+                    <h2 style="margin-top: 0;">Novelty NZ products <span style="color:gray;">Nicky Roundtree</span></h2>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. In imperdiet sed sapien non accumsan. Nullam finibus congue turpis et laoreet. Nulla ex sem, rhoncus in libero vitae, pulvinar ullamcorper risus. Donec tincidunt porttitor sapien id sollicitudin. Ut at convallis nulla. Integer vulputate lobortis velit, nec rutrum libero interdum vitae. Ut tincidunt mauris elit, id imperdiet lacus commodo eget. Quisque quis diam tortor.
+
+                    <p style="font-size: 0.9rem;font-style: italic;"><a href="https://www.flickr.com/photos/32052617@N03/3231534430">"Image"</a><span> by <a href="https://www.flickr.com/photos/32052617@N03">JacksonPF</a></span> is licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/?ref=ccsearch&atype=html" style="margin-right: 5px;">CC BY-NC-SA 2.0</a><a href="https://creativecommons.org/licenses/by-nc-sa/2.0/?ref=ccsearch&atype=html" target="_blank" rel="noopener noreferrer" style="display: inline-block;white-space: none;margin-top: 2px;margin-left: 3px;height: 22px !important;"><img style="height: inherit;margin-right: 3px;display: inline-block;" src="https://search.creativecommons.org/static/img/cc_icon.svg" /><img style="height: inherit;margin-right: 3px;display: inline-block;" src="https://search.creativecommons.org/static/img/cc-by_icon.svg" /><img style="height: inherit;margin-right: 3px;display: inline-block;" src="https://search.creativecommons.org/static/img/cc-nc_icon.svg" /><img style="height: inherit;margin-right: 3px;display: inline-block;" src="https://search.creativecommons.org/static/img/cc-sa_icon.svg" /></a></p>                </div>
                 </div>
             </div>
+        </div>
+        <div id="group_select">
+            Group By: <select id="group_selector" onchange="change_group()">
+                <option value="/">None</option>
+                <option value="/designers">Designer</option>
+            </select>
+
+            <script>
+                function change_group() {
+                    // Changes the page when the group is changed
+                    window.location.href = document.getElementById("group_selector").value + "#group_select"
+                }
+            </script>
         </div>
         <div id="content_list">
             <?php
