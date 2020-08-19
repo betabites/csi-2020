c7d0f02c50bffa33d914f3ae4b6d5140bd3c9ada
Jack Hawinkels <jack.hawinkels@gmail.com>
2020-08-20 09:56:07 +1200

    Corrected some database data

--- Databases/2.3_Databases Assessment/assets/database_data/database.sql
+++ Databases/2.3_Databases Assessment/assets/database_data/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: fdb21.awardspace.net
--- Generation Time: Jul 18, 2020 at 03:14 AM
+-- Generation Time: Aug 19, 2020 at 10:29 PM
 -- Server version: 5.7.20-log
 -- PHP Version: 5.5.38
 
@@ -39,7 +39,7 @@ INSERT INTO `categories` (`category_id`, `category_name`) VALUES
 (1, 'Jewlry'),
 (2, 'Clothing'),
 (3, 'Sports Gear'),
-(4, 'Décor'),
+(4, 'Decor'),
 (5, 'Miscellanious');
 
 -- --------------------------------------------------------
@@ -87,23 +87,23 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `name`, `about`, `designer_id`) VALUES
-(1, 'Awesome Kid Banner', 'Wood banner, painted with Resene Galliano, and cut into a banner shape and engraved. Inspirational and gorgeous home decor item. Approx 13 x 18cm diameter. Twine for hanging.', 1),
+(1, 'Awesome Kid Yellow Banner', 'Wood banner, painted with Resene Galliano, and cut into a banner shape and engraved. Inspirational and gorgeous home decor item. Approx 13 x 18cm diameter. Twine for hanging.', 1),
 (2, 'Be Bold Banner', 'Wood banner, painted, cut and engraved into a flag shape and engraved.Inspirational and quirky home decor item. Approx 17 x 14 cm. Twine for hanging.', 1),
 (3, 'It gets better with Coffee Banner', 'Wood banner, painted with Resene Jaguar, and cut into a banner shape and engraved.Inspirational and gorgeous home decor item. Approx 13 x 19 cm diameter. Twine for hanging', 1),
 (4, 'Day Mountain Pendant', 'Pendants are perfect for dress or casual. Add a splash of colour to a black outfit. Comes in 2 sizes. Hand-painted. Adjustable length chain to suit any neckline. 30 x 30 mm wooden oval face. Hand-painted. Hand-made. Reminiscent of New Zealand\'s Mountain Ranges during the day (also see the night mountain pendant) Aqua colour as per pic.Bronze backing. Nickel and Lead Free Chain. 80cm fully adjustable chain. Comes packaged in a Kraft gift box.', 1),
 (5, 'NZ Natural Pendant', '30 x 40 mm oval face. Hand-painted. Hand-made. NZ design. Natural Rimu Wood. Bronze backing. Nickel and Lead free. 80cm fully adjustable chain.', 1),
 (6, 'Rimu Stag Ring', '25mm diameter. Hand-painted wood face. RIMU. Stag Design. Fully adjustable ring (will go Ser or Lr). Nickel-free Stainless Steel Bronze Coated.', 1),
 (7, 'Tiny Red Mountain Studs', 'Tiny, neat and charming.5mm diameter each earring approx. Rimu wood engraved with mountain design. Hand-painted in red. Surgical stainless steel backs. Gorgeous subtle little earrings for everyday wear.', 1),
-(8, 'Hanging diamonds', 'Beautiful hanging hand-painted wood diamond shaped earrings. Perfect for casual or dress up for a night out. Approx 25mm long, 10mm wide. Wood engraved and hand-painted in yellow. Rimu wood. Nickel-free for sensitive ears.', 1),
-(9, 'Bird Ring', '20 x 27 mm. Hand-painted wood face. Aqua colour. Bird Design. Fully adjustable ring (will go Ser or Lr). Nickel-free Stainless Steel Bronze Coated.', 1),
-(10, 'Paralympics NZ Athletic Unisex Zip Hood', 'Paralympics New Zealand have launched their official Mr Vintage supporters range for the Rio 2016 Paralympic Games.', 2),
-(11, 'NZ Olympic Team Goldie Men\'s Singlet', 'This is the official supporters range for the New Zealand Olympic Team to the 2016 Rio Olympic Games. Brought to you by Mr Vintage, the New Zealand Olympic Committee’s supporters range is the best way to take your support for New Zealand’s 2016 Olympic Team to the next level.', 2),
-(12, 'Paralympics NZ Athletic Men\'s Singlet', 'Paralympics New Zealand have launched their official Mr Vintage supporters range for the Rio 2016 Paralympic Games.', 2),
-(13, 'Bugger Mens T-Shirt', 'Our men\'s tees are 180 GSM, 100% super soft combed cotton, pre-shrunk to minimise shrinkage, lightweight and side-seamed for that tailored cut. Designed specifically for the NZ body shape, we know you\'ll love the fit and feel of the tees as much as what\'s printed on them.', 2),
-(14, 'Burger Queen Womens T-Shirt', 'Our women’s tees are 100% cotton and pre-shrunk to minimise shrinkage, tailored to fit your NZ body. You also now have no reason to say you’ve got “nothing to wear”. This is what you’ll wear', 2),
-(15, 'Tui Love Unisex Graphite Hood', 'Our UNISEX heavyweight hoods are composed of 350 GSM (Nice and warm); 80% Cotton and 20% Polyester. They’ve got a shoe-string cord, a kangaroo pouch for your hands (and lint), a cotton lined hood to look gangsta (if you’re that way inclined), and are made of anti-pill fabric (that’s the good stuff).', 2),
-(16, 'EP: Sugar Skull Women\'s Scoop', 'Originally painted on a skateboard, this design symbolises the colour and finality of life. My love of Mexican artwork has taught me that skulls are not deathly images to be afraid off. Brightly coloured sugar skulls are used to celebrate the lives of loved ones during Día de Muertos (the Day of the Dead festival), they teach us to live and love life. Erika Pearce is an up and coming Auckland artist and designer with a flair for colour and life. Upon leaving her comfy salary-paying graphic design job in August 2012, she has ventured out on her own and has never looked back. Her artwork ranges from tattoo inspired designs to surf and skate art and highly detailed portraits. She is known for her versatility and is in high demand for custom artwork from collectors locally and all over the world. She loves creating things by hand, acrylic paint is her favourite M. Every piece she creates tells a story.', 2),
-(17, 'Wooden Sunnies', 'These unisex, trendy and fun to wear sunglasses are made with wooden arms. Light but sturdy, they come with a microfibre sock case that doubles as a cleaning cloth. Produced by a New Zealand designer these affordable sunglasses are one of our best sellers for all ages!!', 3),
+(8, 'Yellow Hanging diamonds', 'Beautiful hanging hand-painted wood diamond shaped earrings. Perfect for casual or dress up for a night out. Approx 25mm long, 10mm wide. Wood engraved and hand-painted in yellow. Rimu wood. Nickel-free for sensitive ears.', 1),
+(9, 'Aqua Bird Ring', '20 x 27 mm. Hand-painted wood face. Aqua colour. Bird Design. Fully adjustable ring (will go Ser or Lr). Nickel-free Stainless Steel Bronze Coated.', 1),
+(10, 'Paralympics NZ Athletic Black Unisex Zip Hood', 'Paralympics New Zealand have launched their official Mr Vintage supporters range for the Rio 2016 Paralympic Games.', 2),
+(11, 'NZ Olympic Team Goldie Men\'s Black Singlet', 'This is the official supporters range for the New Zealand Olympic Team to the 2016 Rio Olympic Games. Brought to you by Mr Vintage, the New Zealand Olympic Committee�s supporters range is the best way to take your support for New Zealand�s 2016 Olympic Team to the next level.', 2),
+(12, 'Paralympics NZ Athletic Black Men\'s Singlet', 'Paralympics New Zealand have launched their official Mr Vintage supporters range for the Rio 2016 Paralympic Games.', 2),
+(13, 'Bugger Mens Black T-Shirt', 'Our men\'s tees are 180 GSM, 100% super soft combed cotton, pre-shrunk to minimise shrinkage, lightweight and side-seamed for that tailored cut. Designed specifically for the NZ body shape, we know you\'ll love the fit and feel of the tees as much as what\'s printed on them.', 2),
+(14, 'Burger Queen Womens Peach T-Shirt', 'Our women\'s tees are 100% cotton and pre-shrunk to minimise shrinkage, tailored to fit your NZ body. You also now have no reason to say you\'ve got "nothing to wear". This is what you\'ll wear', 2),
+(15, 'Tui Love Unisex Graphite Hood', 'Our UNISEX heavyweight hoods are composed of 350 GSM (Nice and warm); 80% Cotton and 20% Polyester. They�ve got a shoe-string cord, a kangaroo pouch for your hands (and lint), a cotton lined hood to look gangsta (if you�re that way inclined), and are made of anti-pill fabric (that�s the good stuff).', 2),
+(16, 'EP: Sugar Skull Women\'s Scoop', 'Originally painted on a skateboard, this design symbolises the colour and finality of life. My love of Mexican artwork has taught me that skulls are not deathly images to be afraid off. Brightly coloured sugar skulls are used to celebrate the lives of loved ones during Dia de Muertos (the Day of the Dead festival), they teach us to live and love life. Erika Pearce is an up and coming Auckland artist and designer with a flair for colour and life. Upon leaving her comfy salary-paying graphic design job in August 2012, she has ventured out on her own and has never looked back. Her artwork ranges from tattoo inspired designs to surf and skate art and highly detailed portraits. She is known for her versatility and is in high demand for custom artwork from collectors locally and all over the world. She loves creating things by hand, acrylic paint is her favourite M. Every piece she creates tells a story.', 2),
+(17, 'Wooden Sunnies', 'These unisex, trendy and fun to wear sunglasses are made with wooden arms. Light but sturdy, they come with a microfibre sock case that doubles as a cleaning cloth.   Produced by a New Zealand designer these affordable sunglasses are one of our best sellers for all ages!!', 3),
 (18, 'Paddle', 'For the guys and gals who just want to paddle along and enjoy life. Beautifully finished and sporting our attractive Beech Palm Grip, this rugged paddle satisfies the needs of strong paddlers from fishermen to white water rafters. Made in New Zealand.', 3);
 
 -- --------------------------------------------------------
@@ -142,6 +142,27 @@ INSERT INTO `product_categories_link` (`product_id`, `category_id`) VALUES
 (15, 2),
 (16, 2),
 (17, 5),
+(18, 3),
+(1, 4),
+(2, 4),
+(3, 4),
+(4, 1),
+(5, 1),
+(6, 1),
+(7, 1),
+(8, 1),
+(9, 1),
+(10, 2),
+(10, 3),
+(11, 2),
+(11, 3),
+(12, 2),
+(12, 3),
+(13, 2),
+(14, 2),
+(15, 2),
+(16, 2),
+(17, 5),
 (18, 3);
 
 -- --------------------------------------------------------
@@ -159,58 +180,58 @@ CREATE TABLE `product_variations` (
   `img_location` varchar(38) DEFAULT NULL,
   `gender` varchar(1) DEFAULT NULL,
   `inventory` int(2) DEFAULT NULL,
-  `launch_date` date DEFAULT NULL
+  `launch_date` date DEFAULT NULL,
+  `image_mode` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `product_variations`
 --
 
-INSERT INTO `product_variations` (`variation_id`, `product_id`, `price`, `colour`, `size`, `img_location`, `gender`, `inventory`, `launch_date`) VALUES
-(0, NULL, NULL, NULL, 'Replaced sizes with global size abreviations (S, M, L, XL)', NULL, NULL, NULL, NULL),
-(1, 1, 39, 'Yellow', NULL, 'yellowkidsbanner.jpg', NULL, 12, '0000-00-00'),
-(2, 2, 39, 'Blue', NULL, 'boldbanner.jpg', NULL, 12, '0000-00-00'),
-(3, 3, 39, 'Brown', NULL, 'coffeebanner.jpg', NULL, 12, '0000-00-00'),
-(4, 4, 42, 'Blue', NULL, 'mountainpendant.jpg', NULL, 12, '0000-00-00'),
-(5, 5, 42, 'Orange', NULL, 'nzpendant.jpg', NULL, 12, '0000-00-00'),
-(6, 6, 28, 'Brown', NULL, 'stagring.jpg', NULL, 12, '0000-00-00'),
-(7, 7, 29, 'Red', NULL, 'redstuds.png', NULL, 12, '0000-00-00'),
-(8, 8, 34, 'Yellow', NULL, 'yellowdiamonds.jpg', NULL, 12, '0000-00-00'),
-(9, 9, 28, 'Blue', NULL, 'birdring.jpg', NULL, 12, '0000-00-00'),
-(10, 10, 69, 'Black', 'S', 'para-athletic_blk_hood_unisex-back.jpg', 'U', 12, '0000-00-00'),
-(11, 10, 69, 'Black', 'M', 'para-athletic_blk_hood_unisex-back.jpg', 'U', 12, '0000-00-00'),
-(12, 10, 69, 'Black', 'L', 'para-athletic_blk_hood_unisex-back.jpg', 'U', 12, '0000-00-00'),
-(13, 10, 69, 'Black', 'XL', 'para-athletic_blk_hood_unisex-back.jpg', 'U', 12, '0000-00-00'),
-(14, 11, 35, 'Black', 'S', 'nzot-goldie_blk_singlet_mens-back.jpg', 'M', 12, '0000-00-00'),
-(15, 11, 35, 'Black', 'M', 'nzot-goldie_blk_singlet_mens-back.jpg', 'M', 12, '0000-00-00'),
-(16, 11, 35, 'Black', 'L', 'nzot-goldie_blk_singlet_mens-back.jpg', 'M', 12, '0000-00-00'),
-(17, 11, 35, 'Black', 'XL', 'nzot-goldie_blk_singlet_mens-back.jpg', 'M', 12, '0000-00-00'),
-(18, 12, 35, 'Black', 'S', 'para-athletic_blk_singlet_mens.jpg', 'M', 12, '0000-00-00'),
-(19, 12, 35, 'Black', 'M', 'para-athletic_blk_singlet_mens.jpg', 'M', 12, '0000-00-00'),
-(20, 12, 35, 'Black', 'L', 'para-athletic_blk_singlet_mens.jpg', 'M', 12, '0000-00-00'),
-(21, 12, 35, 'Black', 'XL', 'para-athletic_blk_singlet_mens.jpg', 'M', 12, '0000-00-00'),
-(22, 13, 40, 'Black', 'S', 'bugger_blk_tee_mens.jpg', NULL, 12, '0000-00-00'),
-(23, 13, 40, 'Black', 'M', 'bugger_blk_tee_mens.jpg', NULL, 12, '0000-00-00'),
-(24, 13, 40, 'Black', 'L', 'bugger_blk_tee_mens.jpg', NULL, 12, '0000-00-00'),
-(25, 13, 40, 'Black', 'XL', 'bugger_blk_tee_mens.jpg', NULL, 12, '0000-00-00'),
-(26, 14, 40, NULL, 'S', 'burgerqueen_water_tee_womens-main.jpg', 'W', 12, '0000-00-00'),
-(27, 14, 40, NULL, 'M', 'burgerqueen_water_tee_womens-main.jpg', 'W', 12, '0000-00-00'),
-(28, 14, 40, NULL, 'L', 'burgerqueen_water_tee_womens-main.jpg', 'W', 12, '0000-00-00'),
-(29, 14, 40, NULL, 'XL', 'burgerqueen_water_tee_womens-main.jpg', 'W', 12, '0000-00-00'),
-(30, 15, 89, NULL, 'S', 'lh-tuilove_ash_hood_womens.jpg', 'U', 12, '0000-00-00'),
-(31, 15, 89, NULL, 'M', 'lh-tuilove_ash_hood_womens.jpg', 'U', 12, '0000-00-00'),
-(32, 15, 89, NULL, 'L', 'lh-tuilove_ash_hood_womens.jpg', 'U', 12, '0000-00-00'),
-(33, 15, 89, NULL, 'XL', 'lh-tuilove_ash_hood_womens.jpg', 'U', 12, '0000-00-00'),
-(34, 16, 40, NULL, 'S', 'epss-34.jpg', 'W', 12, '0000-00-00'),
-(35, 16, 40, NULL, 'M', 'epss-35.jpg', 'W', 12, '0000-00-00'),
-(36, 16, 40, NULL, 'L', 'epss-36.jpg', 'W', 12, '0000-00-00'),
-(37, 16, 40, NULL, 'XL', 'epss-37.jpg', 'W', 12, '0000-00-00'),
-(38, 17, 39, 'Blue', NULL, 'bluesunnies.jpg', 'U', 12, '0000-00-00'),
-(39, 17, 39, 'Green', NULL, 'greensunnies.jpg', 'U', 12, '0000-00-00'),
-(40, 17, 39, 'Red', NULL, 'redsunnies.jpg', 'U', 12, '0000-00-00'),
-(41, 18, 59, 'Yellow', NULL, 'yellowpaddle.jpg', 'U', 12, '0000-00-00'),
-(42, 18, 59, 'Blue', NULL, 'bluepaddle.jpg', 'U', 12, '0000-00-00'),
-(43, 18, 59, 'Green', NULL, 'greenpaddle.jpg', 'U', 12, '0000-00-00');
+INSERT INTO `product_variations` (`variation_id`, `product_id`, `price`, `colour`, `size`, `img_location`, `gender`, `inventory`, `launch_date`, `image_mode`) VALUES
+(1, 1, 39, 'Yellow', NULL, 'yellowkidsbanner.jpg', NULL, 12, '0000-00-00', 0),
+(2, 2, 39, 'Blue', NULL, 'boldbanner.jpg', NULL, 12, '0000-00-00', 0),
+(3, 3, 39, 'Brown', NULL, 'coffeebanner.jpg', NULL, 12, '0000-00-00', 0),
+(4, 4, 42, 'Blue', NULL, 'mountainpendant.jpg', NULL, 12, '0000-00-00', 0),
+(5, 5, 42, 'Orange', NULL, 'nzpendant.jpg', NULL, 12, '0000-00-00', 0),
+(6, 6, 28, 'Brown', NULL, 'stagring.jpg', NULL, 12, '0000-00-00', 0),
+(7, 7, 29, 'Red', NULL, 'redstuds.png', NULL, 12, '0000-00-00', 0),
+(8, 8, 34, 'Yellow', NULL, 'yellowdiamonds.jpg', NULL, 12, '0000-00-00', 0),
+(9, 9, 28, 'Blue', NULL, 'birdring.jpg', NULL, 12, '0000-00-00', 0),
+(10, 10, 69, 'Black', 'S', 'para-athletic_blk_hood_unisex-back.jpg', 'U', 12, '0000-00-00', 0),
+(11, 10, 69, 'Black', 'M', 'para-athletic_blk_hood_unisex-back.jpg', 'U', 12, '0000-00-00', 0),
+(12, 10, 69, 'Black', 'L', 'para-athletic_blk_hood_unisex-back.jpg', 'U', 12, '0000-00-00', 0),
+(13, 10, 69, 'Black', 'XL', 'para-athletic_blk_hood_unisex-back.jpg', 'U', 12, '0000-00-00', 0),
+(14, 11, 35, 'Black', 'S', 'nzot-goldie_blk_singlet_mens-back.jpg', 'M', 12, '0000-00-00', 0),
+(15, 11, 35, 'Black', 'M', 'nzot-goldie_blk_singlet_mens-back.jpg', 'M', 12, '0000-00-00', 0),
+(16, 11, 35, 'Black', 'L', 'nzot-goldie_blk_singlet_mens-back.jpg', 'M', 12, '0000-00-00', 0),
+(17, 11, 35, 'Black', 'XL', 'nzot-goldie_blk_singlet_mens-back.jpg', 'M', 12, '0000-00-00', 0),
+(18, 12, 35, 'Black', 'S', 'para-athletic_blk_singlet_mens.jpg', 'M', 12, '0000-00-00', 0),
+(19, 12, 35, 'Black', 'M', 'para-athletic_blk_singlet_mens.jpg', 'M', 12, '0000-00-00', 0),
+(20, 12, 35, 'Black', 'L', 'para-athletic_blk_singlet_mens.jpg', 'M', 12, '0000-00-00', 0),
+(21, 12, 35, 'Black', 'XL', 'para-athletic_blk_singlet_mens.jpg', 'M', 12, '0000-00-00', 0),
+(22, 13, 40, 'Black', 'S', 'bugger_blk_tee_mens.jpg', 'M', 12, '0000-00-00', 0),
+(23, 13, 40, 'Black', 'M', 'bugger_blk_tee_mens.jpg', 'M', 12, '0000-00-00', 0),
+(24, 13, 40, 'Black', 'L', 'bugger_blk_tee_mens.jpg', 'M', 12, '0000-00-00', 0),
+(25, 13, 40, 'Black', 'XL', 'bugger_blk_tee_mens.jpg', 'M', 12, '0000-00-00', 0),
+(26, 14, 40, NULL, 'S', 'burgerqueen_water_tee_womens-main.jpg', 'W', 12, '0000-00-00', 0),
+(27, 14, 40, NULL, 'M', 'burgerqueen_water_tee_womens-main.jpg', 'W', 12, '0000-00-00', 0),
+(28, 14, 40, NULL, 'L', 'burgerqueen_water_tee_womens-main.jpg', 'W', 12, '0000-00-00', 0),
+(29, 14, 40, NULL, 'XL', 'burgerqueen_water_tee_womens-main.jpg', 'W', 12, '0000-00-00', 0),
+(30, 15, 89, NULL, 'S', 'lh-tuilove_ash_hood_womens.jpg', 'U', 12, '0000-00-00', 0),
+(31, 15, 89, NULL, 'M', 'lh-tuilove_ash_hood_womens.jpg', 'U', 12, '0000-00-00', 0),
+(32, 15, 89, NULL, 'L', 'lh-tuilove_ash_hood_womens.jpg', 'U', 12, '0000-00-00', 0),
+(33, 15, 89, NULL, 'XL', 'lh-tuilove_ash_hood_womens.jpg', 'U', 12, '0000-00-00', 0),
+(34, 16, 40, NULL, 'S', 'epss-34.jpg', 'W', 12, '0000-00-00', 0),
+(35, 16, 40, NULL, 'M', 'epss-34.jpg', 'W', 12, '0000-00-00', 0),
+(36, 16, 40, NULL, 'L', 'epss-34.jpg', 'W', 12, '0000-00-00', 0),
+(37, 16, 40, NULL, 'XL', 'epss-34.jpg', 'W', 12, '0000-00-00', 0),
+(38, 17, 39, 'Blue', NULL, 'bluesunnies.jpg', 'U', 12, '0000-00-00', 1),
+(39, 17, 39, 'Green', NULL, 'greensunnies.jpg', 'U', 12, '0000-00-00', 1),
+(40, 17, 39, 'Red', NULL, 'redsunnies.jpg', 'U', 12, '0000-00-00', 1),
+(41, 18, 59, 'Yellow', NULL, 'yellowpaddle.jpg', 'U', 12, '0000-00-00', 1),
+(42, 18, 59, 'Blue', NULL, 'bluepaddle.jpg', 'U', 12, '0000-00-00', 1),
+(43, 18, 59, 'Green', NULL, 'greenpaddle.jpg', 'U', 12, '0000-00-00', 1);
 
 --
 -- Indexes for dumped tables
